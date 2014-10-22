5ea81045c896d0542c77b9fd6d59fbf4dc13dca3
in-async <inasync@gmail.com>
2014-10-22 23:18:31 +0900

    作業途中

---
+++ AngularSample/angular/app.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+angular
+    .module('app', [
+        'ngRoute',
+        'app.user'
+    ])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/user'});
+    }])
+;
---
+++ AngularSample/angular/user/user.js
@@ -0,0 +1,41 @@
+﻿'use strict';
+angular
+    .module('app.user', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/user', {
+                templateUrl: 'angular/user/_user.html',
+                controller: 'UserCtrl',
+                controllerAs: 'ctrl',
+            })
+            .when('/user/register', {
+                templateUrl: 'angular/user/_register.html',
+                controller: 'UserCtrl',
+                controllerAs: 'ctrl',
+            })
+        ;
+    }])
+
+    .controller('UserCtrl', [function () {
+        var vm = this;
+
+        vm.users = [
+            { name: '鹿目まどか', gender: 'Female', age: 14, work:'魔法少女' },
+            { name: '暁美ほむら', gender: 'Female', age: 14, work: '魔法少女' },
+        ];
+
+        vm.toggleChecked = function (user) {
+            user.checked = !user.checked;
+        };
+
+        vm.range = function (count) {
+            console.log('range');
+            return new Array(count);
+        };
+
+        vm.create = function (user) {
+            console.log(user);
+        };
+    }])
+;
