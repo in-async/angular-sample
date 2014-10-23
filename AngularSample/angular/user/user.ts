68e5672297f7c6fb0425a41dc0acd31c4e8cb868
in-async <inasync@gmail.com>
2014-10-23 18:00:15 +0900

    作業途中

--- AngularSample/angular/user/user.ts
+++ AngularSample/angular/user/user.ts
@@ -1,28 +1,35 @@
 ﻿/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
 /// <reference path="../../scripts/typings/firebase/firebase.d.ts" />
+
 'use strict';
-angular.module('app.user', ['ngRoute', 'ngResource']).config([
-    '$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/user', {
-            templateUrl: 'angular/user/_user.html',
-            controller: 'UserCtrl',
-            controllerAs: 'ctrl'
-        }).when('/user/register', {
-            templateUrl: 'angular/user/_register.html',
-            controller: 'UserCtrl',
-            controllerAs: 'ctrl'
-        });
-    }]).controller('UserCtrl', [
-    '$resource', function ($resource) {
+angular
+    .module('app.user', ['ngRoute', 'ngResource'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/user', {
+                templateUrl: 'angular/user/_user.html',
+                controller: 'UserCtrl',
+                controllerAs: 'ctrl',
+            })
+            .when('/user/register', {
+                templateUrl: 'angular/user/_register.html',
+                controller: 'UserCtrl',
+                controllerAs: 'ctrl',
+            })
+        ;
+    }])
+
+    .controller('UserCtrl', ['$resource', function ($resource) {
         var vm = this;
 
         var fb = new Firebase('https://angularsample2014.firebaseio.com/users/');
-        fb.on('child_added', function (snapshot) {
+        fb.on('child_added', function (snapshot:IFirebaseDataSnapshot) {
             vm.users.push(snapshot.val());
         });
         vm.users = [
             { name: '鹿目まどか', gender: 'Female', age: 14, work: '魔法少女' },
-            { name: '暁美ほむら', gender: 'Female', age: 14, work: '魔法少女' }
+            { name: '暁美ほむら', gender: 'Female', age: 14, work: '魔法少女' },
         ];
 
         vm.toggleChecked = function (user) {
@@ -40,11 +47,17 @@ angular.module('app.user', ['ngRoute', 'ngResource']).config([
         //    }
         //});
         vm.create = function (user) {
-            fb.push(user);
+            var userRef = fb.push(user);
+
+
         };
         vm.update = function (user) {
+            fb.child
+            fb.update(user);
         };
         vm.delete = function (user) {
+            fb.remove(
         };
-    }]);
-//# sourceMappingURL=user.js.map
+    }])
+;
+ 
